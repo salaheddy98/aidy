3f85040aa0be4abd3dc7f0929ce93d408931bf7d
salaheddy98 <51329942+salaheddy98@users.noreply.github.com>
2019-06-05 12:03:16 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  
+let top;
+let left;
+
+$('.stars').each(function(){
+	$(this).addClass('star'+$(this).index());	
+});
+
+for (var i = 0; i < $('.stars').length; i++) {
+	top = Math.ceil(Math.random()*250);
+	left = Math.ceil(Math.random()*1300);
+	$('.star'+i).css('top',top);
+	$('.star'+i).css('left',left);
+}
+let img_idx = 0;
+for (var i = 1; i <= 8; i++) {
+		
+}
+setInterval(function () { 
+	 img_idx = Math.ceil(Math.random()*8);
+	 console.log(img_idx);
+	$('.milos img').attr('src', 'ricardo/ricardo'+img_idx+'.png');
+}, 1000);
+
+});
